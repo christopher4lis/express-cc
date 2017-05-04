06187f627ef6bd89c5dc6e0b7e49e12b48916409
Christopher Lis <chris@sparxoo.com>
2017-05-03 22:30:19 -0400

    Initial commit.

---
+++ db.js
@@ -0,0 +1,11 @@
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database : process.env.DB_NAME
+})
+
+connection.connect()
+
+module.exports = connection;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const DotenvPlugin = require('webpack-dotenv-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+	entry: {
+		home: './src/home.js',
+	},
+	output: {
+		path: __dirname + '/public/js/',
+		filename: '[name].bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: "css-loader!postcss-loader!sass-loader"
+				}),
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin("../css/style.css"),
+	    new DotenvPlugin({
+			sample: './.env.default',
+			path: './.env'
+	    }),
+	    new BrowserSyncPlugin({
+	        host: 'localhost',
+	        port: 3001,
+	        proxy: 'http://localhost:3000/',
+	        files: ['./views/*.hbs']
+		}),
+	],
+	watch: true,
+	devtool: 'source-map'
+};
