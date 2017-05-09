5c044b7c17b6cef5dfbeb335f921797b4336c826
Christopher Lis <chris@sparxoo.com>
2017-05-09 12:45:16 -0400

    Add babel loader to webpack for ES6.

--- public/js/home.bundle.js
+++ public/js/home.bundle.js
@@ -76,6 +76,9 @@
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
+"use strict";
+
+
 __webpack_require__(0);
 
 /***/ })
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,16 @@ module.exports = {
 					fallback: "style-loader",
 					use: "css-loader!postcss-loader!sass-loader"
 				}),
+			},
+			{
+		        test: /\.js$/,
+		        exclude: /(node_modules|bower_components)/,
+		        use: {
+			        loader: 'babel-loader',
+			        options: {
+				        presets: ['env']
+					}
+				}
 			}
 		]
 	},
