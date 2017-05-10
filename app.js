c4c7f2a7478cf0d93654de2969456a301b645c69
christopher4lis <christopher4lis@gmail.com>
2017-05-09 23:17:45 -0400

    Add dotenv config to app.js.

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var users = require('./routes/users');
 
 var app = express();
 
+require('dotenv').config();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
